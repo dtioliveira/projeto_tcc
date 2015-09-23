b17bb85e7b4f4e9547b775f7d83b6420288f5d63
Diego Oliveira <dti.oliveira@live.com>
2015-09-23 20:43:40 -0300

    Has many associations added

--- app/models/bank.rb
+++ app/models/bank.rb
@@ -1,3 +1,6 @@
 class Bank < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
+
+  has_many :agencies, dependent: :destroy
+  has_many :cities, through: :agencies
 end
--- app/models/city.rb
+++ app/models/city.rb
@@ -1,3 +1,4 @@
 class City < ActiveRecord::Base
   belongs_to :state
+  has_many :agencies, dependent: :destroy
 end
